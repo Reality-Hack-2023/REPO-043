255a031a5b4dfe40516a57ab1f00e69e01a72b48
Steph Ng <recurza@gmail.com>
2023-01-15 12:59:04 -0500

    Push

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    public Camera cam;
+    Vector3 offset;
+
+    private void Start()
+    {
+        cam = Camera.main;
+        offset = cam.transform.position - transform.position;
+    }
+
+    private void Update()
+    {
+        LockPos();
+    }
+
+    public void LockPos()
+    {
+        transform.position = cam.transform.position + cam.transform.forward * offset.magnitude;
+        transform.LookAt(cam.transform, cam.transform.up);
+    }
+}
